b17e76f804ea8d701b96ef7e90ea360b5ee1d189
fboissadier <florent.boissadier@wanadoo.fr>
2024-05-31 02:08:19 +0200

    First integration of hoverStyling on button TODO : Add transition time parameter

---
+++ brXtended/Button/Button.js
@@ -0,0 +1,71 @@
+define(['widgets/brease/Button/Button'], function (SuperClass) {
+    
+    'use strict';
+
+    /**
+     * @class widgets.brXtended.Button
+     * #Description
+     * Extend from widgets.brease.Button 
+     * Adding hover styling properties
+     * @breaseNote 
+     * @extends widgets.brease.Button
+     * @aside example buttons
+    **/
+
+    var defaultSettings = {
+            enableHoverStylingExt: false,
+        },
+
+        WidgetClass = SuperClass.extend(function Button() {
+            SuperClass.apply(this, arguments);
+        }, defaultSettings),
+
+        p = WidgetClass.prototype;
+
+
+
+    p.init = function () {
+        if (this.settings.omitClass !== true) {
+            this.addInitialClass('brXtendedButton');
+            this.settings.omitClass = true;
+        }
+        SuperClass.prototype.init.call(this);
+        this.checkhoverEnabledExtClass(this.settings.enableHoverStylingExt);
+    };
+
+    p.checkhoverEnabledExtClass = function (hoverPropEnable) {
+        console.log(hoverPropEnable);
+        
+        var className = "hoverEnabledExt";
+        console.log(this.el.hasClass(className));
+        if(hoverPropEnable && !this.el.hasClass(className)){
+            this.el.addClass(className);
+        }
+
+        if(!hoverPropEnable && this.el.hasClass(className)){
+            this.el.removeClass(className)
+        }
+    }
+
+    /**
+     * @method setEnableHoverStylingExt
+     * Set enableHoverStylingExt option.
+     * @param {Boolean} value
+     */
+    p.setEnableHoverStylingExt = function setEnableHoverStylingExt(value) {
+        this.settings.enableHoverStylingExt = value;
+        this.checkhoverEnabledExtClass(this.settings.enableHoverStylingExt);
+    };
+
+    /**
+     * @method getEnableHoverStylingExt
+     * Return enableHoverStylingExt option.
+     */
+    p.getEnableHoverStylingExt = function () {
+        return this.settings.enableHoverStylingExt;
+    };
+
+
+    return WidgetClass;
+
+});
